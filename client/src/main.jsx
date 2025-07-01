d9e4be703ae10f17e6e05d894be3116dbff00061
Saurav Pant <sauravpant777@gmail.com>
2025-07-01 16:41:51 +0545

    Add Redux integration and update package dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import { store } from "./store/store.js";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
